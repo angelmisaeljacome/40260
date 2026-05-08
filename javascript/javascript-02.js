c9e9d78fcb9baf9e9ce7796d55242782462c8579
angelmisaeljacome <angelmisaeljacome@gmail.com>
2026-05-08 11:18:39 -0600

    tarea calculadora

--- javascript/javascript-02.js
+++ javascript/javascript-02.js
@@ -14,25 +14,87 @@ for (const key in botones) {
     }
 }
     
+let prm1 = 0;
+let prm2 = 0;
+let tipoOperacion = "";
+let total = 0;
     
 
 function pintar(e){
-    console.log(e.target.innerText)
-    resultado.value += e.target.innerText
+    console.log(e.target.innerText);
+    resultado.value += e.target.innerText;
 }
 
 function pintar2(e){
-    console.log(e.target.innerText)
-    if(e.target.innerText == "+")
-        suma
+    console.log(e.target.innerText);
+    if(e.target.innerText == "+"){
+        prm1 = resultado.value;
+        tipoOperacion = "suma";
+        resultado.value = "";
+    }else if(e.target.innerText == "-"){
+        prm1 = resultado.value;
+        tipoOperacion = "resta";
+        resultado.value = "";
+    }else if(e.target.innerText == "*"){
+        prm1 = resultado.value;
+        tipoOperacion = "multiplicacion";
+        resultado.value = "";
+    }else if(e.target.innerText == "/"){
+        prm1 = resultado.value;
+        tipoOperacion = "division";
+        resultado.value = "";
+    }else if(e.target.innerText == "Ce"){
+        prm1 = 0;
+        prm2 = 0;
+        tipoOperacion = "";
+        total = 0;
+        resultado.value = "";
+    }else if(e.target.innerText == "="){
+        igual()
+    }
+    
 }
 
 function suma(){
-    prm1 = resultado.value
-    resultado.value = ""
+    total = Number(prm1) + Number(prm2);
+    console.log("TotalSum= " + total);
+}
+
+function resta(){
+    total = Number(prm1) - Number(prm2);
+    console.log("TotalRes= " + total);
+}
+
+function multiplicacion(){
+    total = Number(prm1) * Number(prm2);
+    console.log("TotalMulti= " + total);
+}
+
+function division(){
+    total = Number(prm1) / Number(prm2);
+    console.log("TotalDiv= " + total);
 }
 
 function igual(){
-    prm2 = resultado.value
-    resultado.value = prm1 + prm2
+    if (tipoOperacion == "suma"){
+        prm2 = resultado.value;
+        suma()
+        resultado.value = total;
+        
+    }else if(tipoOperacion == "resta"){
+        prm2 = resultado.value;
+        resta()
+        resultado.value = total;
+
+    }else if(tipoOperacion == "multiplicacion"){
+        prm2 = resultado.value;
+        multiplicacion()
+        resultado.value = total;
+
+    }else if(tipoOperacion == "division"){
+        prm2 = resultado.value;
+        division()
+        resultado.value = total;
+
+    }
 }
